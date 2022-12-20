f13bff2d3ab805a6d4ff010837bc1bb4a9ae5bdf
TashixTashi <120760920+TashixTashi@users.noreply.github.com>
2022-12-20 03:14:08 -0500

    add

--- cypress/e2e/Test/addtocart.cy.js
+++ cypress/e2e/Test/addtocart.cy.js
@@ -1,30 +1,80 @@
-import Authentication from '../Page/products.pg'
+import Authenticate from '../Page/products.pg'
 import Product from '../Page/products.pg'
+import Cart from '../Data/cart.pg'
 
-
-it('Verify that a user can add an item to their cart', () => 
-{
-  cy.visit("https://ui-automation-camp.vercel.app/")
-  cy.get("#signInOrRegister").click();
-  cy.origin(
-    "https://dev-mlluudmotpwoldtv.us.auth0.com",
-    { args: {} },
-    ({}) => 
-    {
-     cy.get('[type="email"]').type("tashcamp@yopmail.com");
-     cy.get('[type="password"]').type("P@ssword1!", { log: false });
-     cy.get("[name=submit]").click();  
-     cy.get(Product.getProductId(4))
-   .find(Product.productImgs)
-   .should('be.visible')
-   .click()
-   cy.wait(3000)
-   cy.get(Product.backToProductsBtn).should('be.visible')
-   cy.get(Product.addToCartBtns).click()
-   cy.get(Cart.cartCheckoutBtn).should('be.visible')
-
-   cy.get(Cart.itemInCartName).eq(4).should('have.text', products.products[4].name)
-
+describe('Add-to-cart', () => {
+  beforeEach(() => {
+      cy.visit('/')
+      cy.get(Auth.signInOrRegisterBtn).click();
+      //Login on to site.
+      cy.origin(
+        "https://dev-mlluudmotpwoldtv.us.auth0.com",
+        { args: user },
+        (user) => {
+          cy.get('#1-email').type(user[2].email)
+          cy.get('#1-password').type(user[2].password)
+          cy.get('#1-submit').click()
+        }
+      );
   })
+  it('Verify that a user can add multiple items to their cart', () => {
+    cy.get(Product.getProductId(4))
+    .find(Product.productImgs)
+    .should('be.visible')
+    .click()
+
+    cy.get(Product.backToProductsBtn).should('be.visible')
+    cy.get(Product.addToCartBtns).click()
+    cy.get(Cart.cartCheckoutBtn).should('be.visible')
+
+    cy.get(Cart.cartBackBtn).click()
+    cy.get(Product.backToProductsBtn).click()
+
+    cy.get(Product.getProductId(5))
+    .find(Product.productImgs)
+    .should('be.visible')
+    .click()
+
+    cy.get(Product.backToProductsBtn).should('be.visible')
+    cy.get(Product.addToCartBtns).click()
+
+    cy.get(Cart.itemInCartName).eq(0).should('have.text', products.products[1].name)
+    cy.get(Cart.itemInCartName).eq(1).should('have.text', products.products[0].name)
+
+})
+
+it('Verify that a user can add a single item to the cart', () => {
+  cy.get(Product.getProductId(0))
+  .find(Product.productImgs)
+  .should('be.visible')
+  .click()
+
+  cy.get(Product.backToProductsBtn).should('be.visible')
+  cy.get(Product.addToCartBtns).click()
+  cy.get(Cart.cartCheckoutBtn).should('be.visible')
+
+  cy.get(Cart.itemInCartName).eq(9).should('have.text', products.products[0].name)
+
+
+})
+
+it('Verify that the total increase with each item added', () => {
+  cy.get(Product.getProductId(0))
+  .find(Product.productImgs)
+  .should('be.visible')
+  .click()
+
+  cy.get(Product.backToProductsBtn).should('be.visible')
+  cy.get(Product.addToCartBtns).click()
+  cy.get(Cart.cartCheckoutBtn).should('be.visible')
+
+  cy.get(Cart.cartBackBtn).click()
+  cy.get(Product.addToCartBtns).click()
+
+  cy.get(Cart.cartTotal)
+  .should('be.visible')
+  .contains(`${products.products[0].price+products.products[0].price}`)
+})
+
 
 })
